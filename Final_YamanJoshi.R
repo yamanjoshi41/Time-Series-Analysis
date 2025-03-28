fe85b0ba25a5e3473f7cb008cc1aefa36f693663
Yaman Joshi <yamanjoshi41@gmail.com>
2025-03-28 17:51:34 -0500

    Code

--- Final_YamanJoshi.R
+++ Final_YamanJoshi.R
@@ -1,284 +1,161 @@
-library(fpp3)
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from statsmodels.tsa.stattools import adfuller, kpss
+from statsmodels.tsa.seasonal import STL
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
-data <- readr::read_csv("/Users/yamanjoshi/Downloads/TEMPHELPN.csv")
-data
+# Load data
+data = pd.read_csv("/Users/yamanjoshi/Downloads/TEMPHELPN.csv")
 
+# Prepare date and tsibble equivalent
+data['DATE'] = pd.to_datetime(data['DATE'])
+data = data.set_index('DATE')
+data = data['2000-01-01':]
+data['TEMPHELPN'] = data['TEMPHELPN'].astype(float)
 
-#date preparing
-################################################################################
-
-#tsibble result
-data2 <- data %>%
-  mutate(month = yearmonth(DATE)) %>% 
-  filter(month >= yearmonth("2000 Jan")) %>%  
-  select(-DATE) %>%
-  as_tsibble(index = month)
-data2
-
-view(data2)
-
-
-#plot data
-autoplot(data2, TEMPHELPN) + 
-  labs(y="Thousand of Persons", 
-       title = "Number of people employed in temporary help services")
-
+# Plot data
+data['TEMPHELPN'].plot(title='Number of people employed in temporary help services', ylabel='Thousand of Persons')
+plt.show()
 
 # Using differencing to achieve stationarity in time series data
- data2 %>%
-  transmute(
-    temp_rate = TEMPHELPN,
-    log_temp_rate = log(temp_rate),
-    temp_rate_sd = difference(log(temp_rate), 12),
-    temp_rate_dd = difference(difference(log(temp_rate), 12),1))
+data['log_TEMPHELPN'] = np.log(data['TEMPHELPN'])
+data['temp_rate_sd'] = data['log_TEMPHELPN'].diff(12)
+data['temp_rate_dd'] = data['temp_rate_sd'].diff(1)
 
+# Conduct unit root tests
+def kpss_test(series, **kw):
+    statistic, p_value, n_lags, critical_values = kpss(series, **kw)
+    return p_value
 
-#1.	Conduct unit root tests for the log, the seasonal difference, and the double
-#   difference of the time series. 
-data2 %>%
-  features(log(TEMPHELPN), unitroot_kpss)
+print('KPSS Test for log:', kpss_test(data['log_TEMPHELPN'].dropna(), regression='c'))
+print('KPSS Test for seasonal difference:', kpss_test(data['temp_rate_sd'].dropna(), regression='c'))
+print('KPSS Test for double difference:', kpss_test(data['temp_rate_dd'].dropna(), regression='c'))
 
-data2 %>%
-  features(difference(log(TEMPHELPN), 12), unitroot_kpss)
+# Create plots of the ACF and PACF
+fig, ax = plt.subplots(2, 1, figsize=(12, 8))
+plot_acf(data['temp_rate_sd'].dropna(), lags=36, ax=ax[0])
+plot_pacf(data['temp_rate_sd'].dropna(), lags=36, ax=ax[1])
+ax[0].set_title('Seasonal Difference')
+ax[1].set_title('Partial ACF for Seasonal Difference')
+plt.show()
 
-data2 %>%
-  features(difference(difference(log(TEMPHELPN), 12),1), unitroot_kpss)
+fig, ax = plt.subplots(2, 1, figsize=(12, 8))
+plot_acf(data['temp_rate_dd'].dropna(), lags=36, ax=ax[0])
+plot_pacf(data['temp_rate_dd'].dropna(), lags=36, ax=ax[1])
+ax[0].set_title('Double Difference')
+ax[1].set_title('Partial ACF for Double Difference')
+plt.show()
 
+# Identify two ARIMA models and fit for the time series
+arima1 = ARIMA(data['log_TEMPHELPN'], order=(0, 1, 1), seasonal_order=(0, 1, 1, 12)).fit()
+arima2 = ARIMA(data['log_TEMPHELPN'], order=(1, 1, 0), seasonal_order=(0, 1, 1, 12)).fit()
+auto_arima = ARIMA(data['log_TEMPHELPN'], order=(2, 0, 0), seasonal_order=(0, 1, 1, 12)).fit()
 
+# Compare models
+models = [arima1, arima2, auto_arima]
+model_names = ['ARIMA(0,1,1)(0,1,1)[12]', 'ARIMA(1,1,0)(0,1,1)[12]', 'Auto ARIMA']
+aic_values = [model.aic for model in models]
+bic_values = [model.bic for model in models]
 
+model_comparison = pd.DataFrame({'Model': model_names, 'AIC': aic_values, 'BIC': bic_values})
+print(model_comparison.sort_values(by='AIC'))
 
-################################################################################
-
-
-#2.	Create plots of the ACF and PACF for the seasonal difference and the double 
-#   difference of the log of the time series.
-
-# seasonal difference
-data2 %>%
-  gg_tsdisplay(difference(log(TEMPHELPN), 12),
-               plot_type = 'partial', lag = 36) +
-  labs(title = "Seasonal Difference", y = "")
-
-# double difference
-data2 %>%
-  gg_tsdisplay(difference(difference(log(TEMPHELPN), 12)),
-               plot_type = 'partial', lag = 36) +
-  labs(title = "Double Difference", y = "")
-
-
-################################################################################
-
-
-#3 and 4. Identify two ARIMA models and fit for the time series. 
-
-
-fit <- data2 %>%
-  model(
-    arima1 = ARIMA((log(TEMPHELPN)) ~ pdq(0,1,1) + PDQ(0,1,1)), 
-    arima2 = ARIMA((log(TEMPHELPN)) ~ pdq(1,1,0) + PDQ(0,1,1)),  
-    auto = ARIMA((log(TEMPHELPN)), stepwise = FALSE, approx = FALSE)
-  )
-
-
-fit %>% pivot_longer(everything(), names_to = "Model Name", values_to = "Order")
-
-
-
-#Information criteria
-glance(fit) %>% arrange(AICc) %>% select(.model:BIC)
-
-
-
-################################################################################
-
-#5.	Create a training data set from the start of the series to December 2020.
-
-# Split the data into training and test sets
-train_data <- data2 |>
-  filter_index("2000 Jan" ~ "2020 Dec")
-  train_data <- train_data %>% drop_na()
-
-
-data3 <- train_data %>%
-  mutate(log_temp = log(TEMPHELPN)) %>%
-  select(-TEMPHELPN)
-data3 # THIS IS MY TRAINING DATA NOW
-
-
-test_data <- data2 |>
-  filter_index("2021 Jan" ~ .)
-test_data <- test_data %>% drop_na()
-
-
-
-data4 <- test_data %>%
-  mutate(log_temp = log(TEMPHELPN)) %>%
-  select(-TEMPHELPN)
-data4 # THIS IS MY Testing DATA NOW
-
+# Create training data set
+train_data = data[:'2020-12-31']
+test_data = data['2021-01-01':]
 
 # Fit the "auto" ARIMA model to the training data
-
-best_fit_auto <- data3 %>%
-  model(
-    auto = ARIMA(log_temp ~ pdq(2, 0, 0) + PDQ(0, 1, 1))
-  )
-
+best_fit_auto = ARIMA(train_data['log_TEMPHELPN'], order=(2, 0, 0), seasonal_order=(0, 1, 1, 12)).fit()
 
 # Report the model output
-report(best_fit_auto)
-
-
+print(best_fit_auto.summary())
 
 # Generate residual diagnostics
-best_fit_auto %>%
-  gg_tsresiduals(lag = 36) +
-  labs(title = "Residual Diagnostics for Best ARIMA Model")
-
-
-# Autocorrelation test
-best_fit_auto %>%
-  augment() %>%
-  features(.innov, ljung_box, lag = 24, dof = 3)
-
-
-
-################################################################################
-
-#6.	Fit an ETS model 
-
-# Fit the ETS model to the training data set
-ets_fit <- data3 %>%
-  model(ets = ETS(log_temp))
-report(ets_fit)
-
-
-#residual diagnostic
-ets_fit %>%
-  gg_tsresiduals(lag = 36) +
-  labs(title = "Residual Diagnostics for ETS Model")
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111)
+sns.residplot(best_fit_auto.resid, ax=ax)
+ax.set_title('Residual Diagnostics for Best ARIMA Model')
+plt.show()
 
 # Autocorrelation test
-ets_fit %>%
-  augment() %>%
-  features(.innov, ljung_box, lag = 24, dof = 4)
+from statsmodels.stats.diagnostic import acorr_ljungbox
+ljung_box_result = acorr_ljungbox(best_fit_auto.resid, lags=[24], return_df=True)
+print(ljung_box_result)
 
+# Fit an ETS model
+ets_model = ExponentialSmoothing(train_data['log_TEMPHELPN'], seasonal='add', seasonal_periods=12).fit()
 
+# Report the ETS model output
+print(ets_model.summary())
 
+# Residual diagnostics for ETS model
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111)
+sns.residplot(ets_model.resid, ax=ax)
+ax.set_title('Residual Diagnostics for ETS Model')
+plt.show()
 
-################################################################################
+# Autocorrelation test for ETS
+ljung_box_result_ets = acorr_ljungbox(ets_model.resid, lags=[24], return_df=True)
+print(ljung_box_result_ets)
 
-#7.	Based on the test data set in item 5, compare the ARIMA and the ETS models
+# Compare the ARIMA and the ETS models based on test data set
+arima_forecast = best_fit_auto.get_forecast(steps=len(test_data))
+ets_forecast = ets_model.forecast(steps=len(test_data))
 
+def accuracy(forecast, actual):
+    mape = np.mean(np.abs(forecast - actual) / np.abs(actual))  # MAPE
+    me = np.mean(forecast - actual)  # ME
+    mae = np.mean(np.abs(forecast - actual))  # MAE
+    mpe = np.mean((forecast - actual) / actual)  # MPE
+    return pd.DataFrame({'MAPE': [mape], 'ME': [me], 'MAE': [mae], 'MPE': [mpe]})
 
-bind_rows(
-  best_fit_auto |> accuracy(),
-  ets_fit |> accuracy(),
-  best_fit_auto |> forecast(h = 36) |> accuracy(data4),
-  ets_fit |> forecast(h = 36) |> accuracy(data4)
-) |>
-  select(-ME, -MPE, -ACF1)
+arima_accuracy = accuracy(arima_forecast.predicted_mean, test_data['log_TEMPHELPN'])
+ets_accuracy = accuracy(ets_forecast, test_data['log_TEMPHELPN'])
 
+print('ARIMA Model Accuracy:')
+print(arima_accuracy)
+print('ETS Model Accuracy:')
+print(ets_accuracy)
 
+# Use bootstrapping to generate 1000 simulations of the log of the time series
+stl = STL(data['log_TEMPHELPN'], seasonal=13)
+res = stl.fit()
 
+sim_temp = np.zeros((1000, len(data)))
+for i in range(1000):
+    sim_temp[i, :] = res.trend + np.random.normal(0, res.resid.std(), len(data))
 
+# ETS model for the bootstrapped series
+ets_bootstrapped_forecasts = []
+for i in range(1000):
+    model = ExponentialSmoothing(sim_temp[i, :], seasonal='add', seasonal_periods=12).fit()
+    ets_forecast = model.forecast(steps=36)
+    ets_bootstrapped_forecasts.append(ets_forecast)
 
-################################################################################
+# Plot the ETS bootstrapped forecasts
+plt.figure(figsize=(12, 6))
+for forecast in ets_bootstrapped_forecasts:
+    plt.plot(forecast, color='r', alpha=0.1)
+plt.plot(data['log_TEMPHELPN'], color='b')
+plt.title('Bootstrapped forecasts')
+plt.show()
 
-#8.	Use bootstrapping to generate 1000 simulations of the log of the time series
+# Generate ARIMA bagged forecasts for the three years
+arima_bagged_forecasts = []
+for i in range(1000):
+    model = ARIMA(sim_temp[i, :], order=(2, 0, 0), seasonal_order=(0, 1, 1, 12)).fit()
+    forecast = model.get_forecast(steps=36)
+    arima_bagged_forecasts.append(forecast.predicted_mean)
 
-
-datanew<- data2%>%
-  mutate('LogTEMPHELPN'=log(TEMPHELPN)) %>%
-  select(-TEMPHELPN)
-datanew
-
-temp_stl <- datanew %>%
-  model(stl = STL(LogTEMPHELPN))
-
-temp_stl %>%
-  components() %>%
-  autoplot()
-
-sim_temp <- temp_stl %>%
-  generate(new_data = datanew, times = 1000, bootstrap_block_size = 24) %>%
-  select(-.model, -LogTEMPHELPN)
-sim_temp
-
-ets_forecasts <- sim_temp %>%
-  model(ets = ETS(.sim)) %>% 
-  forecast(h = 36)
-
-ets_forecasts |>
-  update_tsibble(key = .rep) |>
-  autoplot(.mean) +
-  autolayer(datanew,LogTEMPHELPN) +
-  guides(colour = "none") +
-  labs(title = "Bootstrapped forecasts",
-       y="-LogTEMPHELPN")
-
-
-
-
-################################################################################
-
-#9.	Create a plot that shows three-year forecasts of the better model 
-
-#bagged mean for the ETS bootstrapped forecasts
-bagged_ets <- ets_forecasts %>%
-  summarise(bagged_mean = mean(.mean))  
-
-# Print the bagged mean for the first 36 periods 
-print(bagged_ets, n = 36)
-
-
-#print the tsibble that shows the ETS bagged forecasts for the three years 
-
-ets_forecast <- datanew%>%
-  model(ets = ETS(LogTEMPHELPN)) %>%
-  forecast(h = 36)
-
-
-# Plotting the results
-ets_forecast %>%
-  autoplot(datanew) +  
-  autolayer(bagged_ets, bagged_mean, col = "#D55E00") + 
-  labs(title = "Comparing bagged ETS forecasts and ETS model",
-       y = "thousabd of people")
-
-
-
-#10 . Print the tsibble that shows the ARIMA bagged forecasts for the three years 
-arima_forecasts <- sim_temp %>%
-  model(arima = ARIMA(.sim ~ 0 + pdq(2,0,0) + PDQ(0,1,1))) %>%
-  forecast(h = 36)
-warnings(50)
-
-arima_forecasts %>%
-  update_tsibble(key = .rep) %>%
-  autoplot(.mean) +
-  autolayer(datanew,LogTEMPHELPN) +
-  guides(colour = "none") +
-  labs(title = "Bootstrapped forecasts",
-       y="-LogTEMPHELPN")
-
-bagged_arima <- arima_forecasts %>%
-  summarise(bagged_mean = mean(.mean))
-
-print(bagged_arima, n = 36)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Plot ARIMA bagged forecasts
+plt.figure(figsize=(12, 6))
+for forecast in arima_bagged_forecasts:
+    plt.plot(forecast, color='r', alpha=0.1)
+plt.plot(data['log_TEMPHELPN'], color='b')
+plt.title('ARIMA Bootstrapped forecasts')
+plt.show()
